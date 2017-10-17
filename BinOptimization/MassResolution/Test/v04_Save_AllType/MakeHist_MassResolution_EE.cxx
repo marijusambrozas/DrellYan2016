55eb8f6a606ca8abc760e9cec4e57f665a2808e3
Kyoengpil Lee <kplee@cern.ch>
2017-10-17 18:08:30 +0900

    save all kinds of rel.diff values

---
+++ BinOptimization/MassResolution/Test/v04_Save_AllType/MakeHist_MassResolution_EE.cxx
@@ -0,0 +1,13 @@
+#include <BinOptimization/MassResolution/Test/v03_dressed_vs_Reco/MakeHist_MassResolution.h>
+
+void MakeHist_MassResolution_EE( TString FileName_ROOTFileList, TString Tag, Int_t IsMC, Double_t NormFactor )
+{
+	HistProducer *producer = new HistProducer( FileName_ROOTFileList, Tag, IsMC );
+	producer->Set_NormFactor( NormFactor );
+	producer->Set_ChannelType( "EE" );
+	producer->Produce();
+
+	TFile *f_output = TFile::Open("ROOTFile_MakeHist_MassResolution_EE.root", "RECREATE");
+	producer->Save( f_output );
+	f_output->Close();
+}
