c055a3aedfac5cbdba75264c6440a64623a3d6e5
Marijus Ambrozas <mambroza@ui10.sdfarm.kr>
2018-10-02 20:43:07 +0900

    Almost working

--- SelectedX/Execute/SelectAllX.sh
+++ SelectedX/Execute/SelectAllX.sh
@@ -5,26 +5,23 @@ if [ ${#1} -eq 0 ] || [ ${#2} -eq 0 ] ; then
     exit
 fi
 
-WhichX='\"'$1'\"'
+WhichX=$1
 #echo $WhichX
-Trigger='\"'$3'\"'
+Trigger=$3
 #echo $Trigger
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram p CMSSW CMSSW_8_0_6&
 wait %1
 cd CMSSW_8_0_6/src
-eval ‘scram runtime -sh‘
+eval `scram runtime -sh`
 cd /cms/ldap_home/mambroza/DrellYan2016/SelectedX/
 
 
-Process='\"'$2'\"'
-#echo "root -l -q -b MakeSelectedX.C($WhichX, $Process, $Trigger)"
-root -l -q -b MakeSelectedX.C($WhichX, $Process, $Trigger)
+Process=$2
+root -l -q -b MakeSelectedX.C'("'$WhichX'", "'$Process'", "'$Trigger'")'
 if [[ $Process == *'QCDEM'* ]] ; then
-#    echo "root -l -q -b MakeSelectedX.C(\\\"QCDfail\\\", \\\"\\\", $Trigger)"&
-    root -l -q -b MakeSelectedX.C(\\\"QCDfail\\\", \\\"\\\", $Trigger)&
+    root -l -q -b MakeSelectedX.C'("QCDfail", "", "'$Trigger'")'&
     wait %1
-#    echo "root -l -q -b MakeSelectedX.C(\\\"QCDmerge\\\", \\\"\\\", $Trigger)"
-    root -l -q -b MakeSelectedX.C(\\\"QCDfail\\\", \\\"\\\", $Trigger)
+    root -l -q -b MakeSelectedX.C'("QCDmerge", "", "")'
 fi
