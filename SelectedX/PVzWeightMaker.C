ab93fb2370b4b02b4491ed84f8b71bd087921560
marijusambrozas <marijus.ambrozas@gmail.com>
2019-09-10 11:03:18 +0300

    Separated the code into several parts (otherwise uses too much RAM)

--- SelectedX/PVzWeightMaker.C
+++ SelectedX/PVzWeightMaker.C
@@ -28,35 +28,51 @@
 #include "./header/myProgressBar_t.h"
 #include "./header/LocalFileMgr.h"
 
-void PVzWeightMaker ()
+
+void PVzWeightMaker_ee();
+void PVzWeightMaker_mumu();
+void PVzWeightMaker_emu();
+void PVzWeightMaker_merge();
+
+void PVzWeightMaker(TString whichX)
 {
-    // -- Run2016 luminosity [/pb] -- //
-    Double_t L_B2F = 19721.0, L_G2H = 16146.0, L_B2H = 35867.0, L = 0;
-    L = L_B2H;
+    TString WhichX = whichX;
+    WhichX.ToUpper();
 
     TTimeStamp ts_start;
     cout << "[Start Time(local time): " << ts_start.AsString("l") << "]" << endl;
     TStopwatch totaltime;
     totaltime.Start();
 
-    LocalFileMgr Mgr;
+    if (WhichX.Contains("EE")) PVzWeightMaker_ee();
+    if (WhichX.Contains("MUMU")) PVzWeightMaker_mumu();
+    if (WhichX.Contains("EMU")) PVzWeightMaker_emu();
+    if (WhichX.Contains("MERGE")) PVzWeightMaker_merge();
 
-    TFile *f = new TFile("./etc/PVzWeights.root", "RECREATE");
+    Double_t TotalRunTime = totaltime.CpuTime();
+    cout << "Total RunTime: " << TotalRunTime << " seconds" << endl;
 
-    TH1D* h_PVz_MC_ee = new TH1D("h_PVz_MC_ee", "", 80, -20, 20);
-    TH1D* h_PVz_Data_ee = new TH1D("h_PVz_Data_ee", "", 80, -20, 20);
-    TH1D* h_PVz_MC_mumu = new TH1D("h_PVz_MC_mumu", "", 80, -20, 20);
-    TH1D* h_PVz_Data_mumu = new TH1D("h_PVz_Data_mumu", "", 80, -20, 20);
-    TH1D* h_PVz_MC_emu = new TH1D("h_PVz_MC_emu", "", 80, -20, 20);
-    TH1D* h_PVz_Data_emu = new TH1D("h_PVz_Data_emu", "", 80, -20, 20);
+    TTimeStamp ts_end;
+    cout << "[End Time(local time): " << ts_end.AsString("l") << "]" << endl;
+
+} // End of PVzWeightMaker()
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // ------------------------------------ EE PART -----------------------------------------//
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+void PVzWeightMaker_ee()
+{
+    LocalFileMgr Mgr;
+
+    TFile *f = new TFile("./etc/PVzWeights_ee.root", "RECREATE");
+
+    TH1D* h_PVz_MC_ee = new TH1D("h_PVz_MC_ee", "", 80, -20, 20);
+    TH1D* h_PVz_Data_ee = new TH1D("h_PVz_Data_ee", "", 80, -20, 20);
+
 // -------------------------------------- MC --------------------------------------------//
-/*    for (SelProc_t pr=_EE_DY_10to50; pr<_EndOf_EE_WJets; pr=Next(pr))
+    for (SelProc_t pr=_EE_DY_10to50; pr<_EndOf_EE_WJets_Normal; pr=next(pr))
     {
         Mgr.SetProc(pr);
         cout << "Process: " << Mgr.Procname[Mgr.CurrentProc] << endl;
@@ -96,7 +112,7 @@ void PVzWeightMaker ()
                 if(EE->isSelPassed == kTRUE)
                 {
                     // -- Normalization -- //
-                    Double_t TotWeight = (L * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * EE->GENEvt_weight;
+                    Double_t TotWeight = (Lumi * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * EE->GENEvt_weight;
 
                     // -- Pileup-Reweighting -- //
                     Double_t PUWeight = analyzer->PileUpWeightValue_80X(EE->nPileUp);
@@ -115,7 +131,7 @@ void PVzWeightMaker ()
 
 
 // ------------------------------------- Data -------------------------------------------//
-    for (SelProc_t pr=_EE_DoubleEG_B; pr<_EndOf_EE_DoubleEG_Normal; pr=Next(pr))
+    for (SelProc_t pr=_EE_DoubleEG_B; pr<_EndOf_EE_DoubleEG_Normal; pr=next(pr))
     {
         Mgr.SetProc(pr);
         cout << "Process: " << Mgr.Procname[Mgr.CurrentProc] << endl;
@@ -149,11 +165,33 @@ void PVzWeightMaker ()
             } // End of event iteration
         }// End of i_tup iteration
     }// End of process iteration
-*/
+
+    h_PVz_MC_ee->SetDirectory(0);
+    h_PVz_Data_ee->SetDirectory(0);
+
+    // -- Writing -- //
+    f->cd();
+    h_PVz_MC_ee->Write();
+    h_PVz_Data_ee->Write();
+    f->Close();
+
+    if (!f->IsOpen()) cout << "File PVzWeights_ee.root has been closed successfully.\n" << endl;
+    else cout << "FILE PVzWeights_ee.root COULD NOT BE CLOSED!\n" << endl;
+
+} // End of PVzWeightMaker_ee
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // ----------------------------------- MuMu PART ----------------------------------------//
 ///////////////////////////////////////////////////////////////////////////////////////////
+
+void PVzWeightMaker_mumu()
+{
+    LocalFileMgr Mgr;
+
+    TFile *f = new TFile("./etc/PVzWeights_mumu.root", "RECREATE");
+
+    TH1D* h_PVz_MC_mumu = new TH1D("h_PVz_MC_mumu", "", 80, -20, 20);
+    TH1D* h_PVz_Data_mumu = new TH1D("h_PVz_Data_mumu", "", 80, -20, 20);
 
 // -------------------------------------- MC --------------------------------------------//
 
@@ -172,8 +210,6 @@ void PVzWeightMaker ()
         // -- For efficiency SF -- //
         analyzer->SetupEfficiencyScaleFactor_BtoF();
         analyzer->SetupEfficiencyScaleFactor_GtoH();
-        analyzer->SetupEfficiencyScaleFactor_BtoF_new();
-        analyzer->SetupEfficiencyScaleFactor_GtoH_new();
 
         //Loop for all samples
         const Int_t Ntup = Mgr.FileLocation.size();
@@ -201,17 +237,15 @@ void PVzWeightMaker ()
                 if(MuMu->isSelPassed == kTRUE)
                 {
                     // -- Normalization -- //
-                    Double_t TotWeight = (L * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * MuMu->GENEvt_weight;
+                    Double_t TotWeight = (Lumi * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * MuMu->GENEvt_weight;
 
                     // -- Pileup-Reweighting -- //
                     Double_t PUWeight = analyzer->PileUpWeightValue_80X(MuMu->nPileUp);
 
                     // -- efficiency weights -- //
-//                    Double_t weight1 = analyzer->EfficiencySF_EventWeight_HLT_BtoF(MuMu);
-//                    Double_t weight2 = analyzer->EfficiencySF_EventWeight_HLT_GtoH(MuMu);
-                    Double_t weight1 = analyzer->EfficiencySF_EventWeight_HLT_BtoF_new(MuMu);
-                    Double_t weight2 = analyzer->EfficiencySF_EventWeight_HLT_GtoH_new(MuMu);
-                    Double_t effweight = (L_B2F * weight1 + L_G2H * weight2) / L_B2H;
+                    Double_t weight1 = analyzer->EfficiencySF_EventWeight_HLT_BtoF(MuMu);
+                    Double_t weight2 = analyzer->EfficiencySF_EventWeight_HLT_GtoH(MuMu);
+                    Double_t effweight = (Lumi_BtoF * weight1 + Lumi_GtoH * weight2) / Lumi;
 
                     h_PVz_MC_mumu->Fill(MuMu->PVz, TotWeight * PUWeight * effweight);
                 }
@@ -258,10 +292,32 @@ void PVzWeightMaker ()
         }// End of i_tup iteration
     } // End of process iteration
 
+    h_PVz_MC_mumu->SetDirectory(0);
+    h_PVz_Data_mumu->SetDirectory(0);
+
+    // -- Writing -- //
+    f->cd();
+    h_PVz_MC_mumu->Write();
+    h_PVz_Data_mumu->Write();
+    f->Close();
+
+    if (!f->IsOpen()) cout << "File PVzWeights_mumu.root has been closed successfully.\n" << endl;
+    else cout << "FILE PVzWeights_mumu.root COULD NOT BE CLOSED!\n" << endl;
+
+} // End of PVzWeightMaker_mumu
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // ------------------------------------ EMu PART ----------------------------------------//
 ///////////////////////////////////////////////////////////////////////////////////////////
+
+void PVzWeightMaker_emu()
+{
+    LocalFileMgr Mgr;
+
+    TFile *f = new TFile("./etc/PVzWeights_emu.root", "RECREATE");
+
+    TH1D* h_PVz_MC_emu = new TH1D("h_PVz_MC_emu", "", 80, -20, 20);
+    TH1D* h_PVz_Data_emu = new TH1D("h_PVz_Data_emu", "", 80, -20, 20);
 
 // -------------------------------------- MC --------------------------------------------//
 
@@ -308,7 +364,7 @@ void PVzWeightMaker ()
                 if(EMu->isSelPassed == kTRUE)
                 {
                     // -- Normalization -- //
-                    Double_t TotWeight = (L * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * EMu->GENEvt_weight;
+                    Double_t TotWeight = (Lumi * Mgr.Xsec[i_tup] / Mgr.Wsum[i_tup]) * EMu->GENEvt_weight;
 
                     // -- Pileup-Reweighting -- //
                     Double_t PUWeight = analyzer->PileUpWeightValue_80X(EMu->nPileUp);
@@ -316,7 +372,7 @@ void PVzWeightMaker ()
                     // -- efficiency weights -- //
                     Double_t weight1 = analyzer->EfficiencySF_EventWeight_emu_BtoF(EMu);
                     Double_t weight2 = analyzer->EfficiencySF_EventWeight_emu_GtoH(EMu);
-                    Double_t effweight = (L_B2F*weight1 + L_G2H*weight2)/L_B2H;
+                    Double_t effweight = (Lumi_BtoF*weight1 + Lumi_GtoH*weight2)/Lumi;
 
                     h_PVz_MC_emu->Fill(EMu->PVz, TotWeight * PUWeight * effweight);
                 }// End of event selection
@@ -364,34 +420,63 @@ void PVzWeightMaker ()
         }// End of i_tup iteration
     }// End of process iteration
 
-// ------------------------------------ FINAL STEPS -------------------------------------//
+    h_PVz_MC_emu->SetDirectory(0);
+    h_PVz_Data_emu->SetDirectory(0);
+
+    // -- Writing -- //
+    f->cd();
+    h_PVz_MC_emu->Write();
+    h_PVz_Data_emu->Write();
+    f->Close();
+
+    if (!f->IsOpen()) cout << "File PVzWeights_emu.root has been closed successfully.\n" << endl;
+    else cout << "FILE PVzWeights_emu.root COULD NOT BE CLOSED!\n" << endl;
+
+} // End of PVzWeightMaker_emu
+
+// ------------------------------------ MERGER -------------------------------------//
+void PVzWeightMaker_merge()
+{
+    LocalFileMgr Mgr;
+
+    TFile *f_ee = new TFile("./etc/PVzWeights_ee.root", "READ");
+    TFile *f_mumu = new TFile("./etc/PVzWeights_mumu.root", "READ");
+    TFile *f_emu = new TFile("./etc/PVzWeights_emu.root", "READ");
+    TFile *f = new TFile("./etc/PVzWeights.root", "RECREATE");
+
+    TH1D* h_PVz_MC_ee = (TH1D*)(f_ee->Get("h_PVz_MC_ee"));
+    TH1D* h_PVz_Data_ee = (TH1D*)(f_ee->Get("h_PVz_Data_ee"));
+    TH1D* h_PVz_MC_mumu = (TH1D*)(f_mumu->Get("h_PVz_MC_mumu"));
+    TH1D* h_PVz_Data_mumu = (TH1D*)(f_mumu->Get("h_PVz_Data_mumu"));
+    TH1D* h_PVz_MC_emu = (TH1D*)(f_emu->Get("h_PVz_MC_emu"));
+    TH1D* h_PVz_Data_emu = (TH1D*)(f_emu->Get("h_PVz_Data_emu"));
 
     // -- Scaling histograms to have integral equal to 1 -- //
-//    h_PVz_MC_ee->Scale(1/h_PVz_MC_ee->Integral(1, h_PVz_MC_ee->GetSize()-2));
-//    h_PVz_Data_ee->Scale(1/h_PVz_Data_ee->Integral(1, h_PVz_Data_ee->GetSize()-2));
-    h_PVz_MC_mumu->Scale(1/h_PVz_MC_mumu->Integral(1, h_PVz_MC_mumu->GetSize()-2));
-    h_PVz_Data_mumu->Scale(1/h_PVz_Data_mumu->Integral(1, h_PVz_Data_mumu->GetSize()-2));
-    h_PVz_MC_emu->Scale(1/h_PVz_MC_emu->Integral(1, h_PVz_MC_emu->GetSize()-2));
-    h_PVz_Data_emu->Scale(1/h_PVz_Data_emu->Integral(1, h_PVz_Data_emu->GetSize()-2));
+    h_PVz_MC_ee->Scale(1/h_PVz_MC_ee->Integral());
+    h_PVz_Data_ee->Scale(1/h_PVz_Data_ee->Integral());
+    h_PVz_MC_mumu->Scale(1/h_PVz_MC_mumu->Integral());
+    h_PVz_Data_mumu->Scale(1/h_PVz_Data_mumu->Integral());
+    h_PVz_MC_emu->Scale(1/h_PVz_MC_emu->Integral());
+    h_PVz_Data_emu->Scale(1/h_PVz_Data_emu->Integral());
 
     // -- Creating weight histograms -- //
-//    TH1D* h_PVzWeights_ee = ((TH1D*)(h_PVz_Data_ee->Clone("h_PVzWeights_ee")));
+    TH1D* h_PVzWeights_ee = ((TH1D*)(h_PVz_Data_ee->Clone("h_PVzWeights_ee")));
     TH1D* h_PVzWeights_mumu = ((TH1D*)(h_PVz_Data_mumu->Clone("h_PVzWeights_mumu")));
     TH1D* h_PVzWeights_emu = ((TH1D*)(h_PVz_Data_emu->Clone("h_PVzWeights_emu")));
 
-//    h_PVzWeights_ee->Divide(h_PVz_MC_ee);
+    h_PVzWeights_ee->Divide(h_PVz_MC_ee);
     h_PVzWeights_mumu->Divide(h_PVz_MC_mumu);
     h_PVzWeights_emu->Divide(h_PVz_MC_emu);
 
     // -- Drawing -- //
-//    TCanvas *c_ee = new TCanvas("ee", "ee", 800, 800);
-//    h_PVz_MC_ee->SetMarkerStyle(kFullDotLarge);
-//    h_PVz_Data_ee->SetMarkerStyle(kFullDotLarge);
-//    h_PVz_MC_ee->SetMarkerColor(kRed);
-//    h_PVz_Data_ee->SetMarkerStyle(kBlack);
-//    h_PVz_MC_ee->Draw();
-//    h_PVz_Data_ee->Draw("SAME");
-//    c_ee->Update();
+    TCanvas *c_ee = new TCanvas("ee", "ee", 800, 800);
+    h_PVz_MC_ee->SetMarkerStyle(kFullDotLarge);
+    h_PVz_Data_ee->SetMarkerStyle(kFullDotLarge);
+    h_PVz_MC_ee->SetMarkerColor(kRed);
+    h_PVz_Data_ee->SetMarkerStyle(kBlack);
+    h_PVz_MC_ee->Draw();
+    h_PVz_Data_ee->Draw("SAME");
+    c_ee->Update();
 
     TCanvas *c_mumu = new TCanvas("mumu", "mumu", 800, 800);
     h_PVz_MC_mumu->SetMarkerStyle(kFullDotLarge);
@@ -412,22 +497,22 @@ void PVzWeightMaker ()
     c_emu->Update();
 
     TCanvas *c_weights = new TCanvas("weights", "weights", 800, 800);
-//    h_PVzWeights_ee->SetMarkerStyle(kFullDotLarge);
+    h_PVzWeights_ee->SetMarkerStyle(kFullDotLarge);
     h_PVzWeights_mumu->SetMarkerStyle(kFullDotLarge);
     h_PVzWeights_emu->SetMarkerStyle(kFullDotLarge);
-//    h_PVzWeights_ee->SetMarkerColor(kBlack);
+    h_PVzWeights_ee->SetMarkerColor(kBlack);
     h_PVzWeights_mumu->SetMarkerColor(kRed);
     h_PVzWeights_emu->SetMarkerColor(kBlue);
-//    h_PVzWeights_ee->Draw();
-    h_PVzWeights_mumu->Draw();
+    h_PVzWeights_ee->Draw();
+    h_PVzWeights_mumu->Draw("SAME");
     h_PVzWeights_emu->Draw("SAME");
     c_weights->Update();
 
-//    h_PVzWeights_ee->SetDirectory(0);
+    h_PVzWeights_ee->SetDirectory(0);
     h_PVzWeights_mumu->SetDirectory(0);
     h_PVzWeights_emu->SetDirectory(0);
-//    h_PVz_MC_ee->SetDirectory(0);
-//    h_PVz_Data_ee->SetDirectory(0);
+    h_PVz_MC_ee->SetDirectory(0);
+    h_PVz_Data_ee->SetDirectory(0);
     h_PVz_MC_mumu->SetDirectory(0);
     h_PVz_Data_mumu->SetDirectory(0);
     h_PVz_MC_emu->SetDirectory(0);
@@ -435,13 +520,7 @@ void PVzWeightMaker ()
 
     // -- Writing -- //
     f->cd();
-//    h_PVz_MC_ee->Write();
-//    h_PVz_Data_ee->Write();
-    h_PVz_MC_mumu->Write();
-    h_PVz_Data_mumu->Write();
-    h_PVz_MC_emu->Write();
-    h_PVz_Data_emu->Write();
-//    h_PVzWeights_ee->Write();
+    h_PVzWeights_ee->Write();
     h_PVzWeights_mumu->Write();
     h_PVzWeights_emu->Write();
     f->Close();
@@ -449,10 +528,4 @@ void PVzWeightMaker ()
     if (!f->IsOpen()) cout << "File PVzWeights.root has been closed successfully.\n" << endl;
     else cout << "FILE PVzWeights.root COULD NOT BE CLOSED!\n" << endl;
 
-    Double_t TotalRunTime = totaltime.CpuTime();
-    cout << "Total RunTime: " << TotalRunTime << " seconds" << endl;
-
-    TTimeStamp ts_end;
-    cout << "[End Time(local time): " << ts_end.AsString("l") << "]" << endl;
-
-} // End of PVzWeightMaker.C
+} // End of PVzWeightMaker_merge
