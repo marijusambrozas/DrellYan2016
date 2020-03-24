0ec9549de9421dfb2e513439da8bb5015c544e1b
marijusambrozas <marijus.ambrozas@gmail.com>
2020-03-24 15:13:36 +0200

    double->float for trigPS

--- SelectedX/header/NtupleHandle.h
+++ SelectedX/header/NtupleHandle.h
@@ -24,7 +24,7 @@ public:
     Int_t HLT_ntrig;
     Int_t HLT_trigFired[MaxN];
     vector<string> *HLT_trigName;
-    Int_t HLT_trigPS[MaxN];
+    float HLT_trigPS[MaxN];
     Double_t HLT_trigPt[MaxN];
     Double_t HLT_trigEta[MaxN];
     Double_t HLT_trigPhi[MaxN];
