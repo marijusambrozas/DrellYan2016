b072ebbcbcb2849cfe8f54af72aa048ca10202c4
marijusambrozas <marijus.ambrozas@gmail.com>
2020-04-15 15:07:20 +0300

    Bug fixes

--- SelectedX/FR_PrescaleTest_t3.C
+++ SelectedX/FR_PrescaleTest_t3.C
@@ -84,139 +84,135 @@ void FR_PrescaleTest_t3 (Bool_t DEBUG = kFALSE)
         for(Int_t i=0; i<NEvents; i++)
         {
             chain->GetEntry(i);
-            if (DEBUG == kTRUE){
-                cout << "\nEvt " << i << endl;
-                cout << "nEle = " << p_T->size() << endl;
-                cout << "p_T[1] = " << p_T->at(0) << endl;
+            Int_t matched22=0, matched30=0, matched36=0, matched50=0, matched75=0, matched90=0, matched120=0, matched175=0;
+            Int_t i_22=-1, i_30=-1, i_36=-1, i_50=-1, i_75=-1, i_90=-1, i_120=-1, i_175=-1;
+
+            for (Int_t i_tr=0; i_tr<ntuple->HLT_ntrig; i_tr++)
+            {
+                if (!(ntuple->HLT_trigFired[i_tr])) continue;
+                if (!((TString)(ntuple->HLT_trigName->at(i_tr))).Contains("Photon")) continue;
+                if (ntuple->HLT_trigPt[i_tr] <= 22) continue;
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon22_v*" && ntuple->HLT_trigPt[i_tr] > 22 && ntuple->HLT_trigPt[i_tr] <= 30)
+                {
+                    i_22 = i_tr;
+                    matched22 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon30_v*" && ntuple->HLT_trigPt[i_tr] > 30 && ntuple->HLT_trigPt[i_tr] <= 36)
+                {
+                    i_30 = i_tr;
+                    matched30 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon36_v*" && ntuple->HLT_trigPt[i_tr] > 36 && ntuple->HLT_trigPt[i_tr] <= 50)
+                {
+                    i_36 = i_tr;
+                    matched36 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon50_v*" && ntuple->HLT_trigPt[i_tr] > 50 && ntuple->HLT_trigPt[i_tr] <= 75)
+                {
+                    i_50 = i_tr;
+                    matched50 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon75_v*" && ntuple->HLT_trigPt[i_tr] > 75 && ntuple->HLT_trigPt[i_tr] <= 90)
+                {
+                    i_75 = i_tr;
+                    matched75 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon90_v*" && ntuple->HLT_trigPt[i_tr] > 90 && ntuple->HLT_trigPt[i_tr] <= 120)
+                {
+                    i_90 = i_tr;
+                    matched90 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon120_v*" && ntuple->HLT_trigPt[i_tr] > 120 && ntuple->HLT_trigPt[i_tr] <= 175)
+                {
+                    i_120 = i_tr;
+                    matched120 = 1;
+                }
+                if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon175_v*" && ntuple->HLT_trigPt[i_tr] > 175)
+                {
+                    i_175 = i_tr;
+                    matched175 = 1;
+                }
+
             }
-
-                for (Int_t i_tr=0; i_tr<ntuple->HLT_ntrig; i_tr++)
-                {
-                    if (!(ntuple->HLT_trigFired[i_tr])) continue;
-                    if (!(ntuple->HLT_trigName->at(i_tr)).Contains("Photon")) continue;
-                    if (ntuple->HLT_trigPt[i_tr] <= 22) continue;
-                    Int_t matched22=0, matched30=0, matched36=0, matched50=0, matched75=0, matched90=0, matched120=0, matched175=0;
-                    Int_t i_22=-1, i_30=-1, i_36=-1, i_50=-1, i_75=-1, i_90=-1, i_120=-1, i_175=-1;
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon22_v*" && ntuple->HLT_trigPt[i_tr] > 22 && ntuple->HLT_trigPt[i_tr] <= 30)
-                    {
-                        i_22 = i_tr;
-                        matched22 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon30_v*" && ntuple->HLT_trigPt[i_tr] > 30 && ntuple->HLT_trigPt[i_tr] <= 36)
-                    {
-                        i_30 = i_tr;
-                        matched30 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon36_v*" && ntuple->HLT_trigPt[i_tr] > 36 && ntuple->HLT_trigPt[i_tr] <= 50)
-                    {
-                        i_36 = i_tr;
-                        matched36 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon50_v*" && ntuple->HLT_trigPt[i_tr] > 50 && ntuple->HLT_trigPt[i_tr] <= 75)
-                    {
-                        i_50 = i_tr;
-                        matched50 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon75_v*" && ntuple->HLT_trigPt[i_tr] > 75 && ntuple->HLT_trigPt[i_tr] <= 90)
-                    {
-                        i_75 = i_tr;
-                        matched75 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon90_v*" && ntuple->HLT_trigPt[i_tr] > 90 && ntuple->HLT_trigPt[i_tr] <= 120)
-                    {
-                        i_90 = i_tr;
-                        matched90 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon120_v*" && ntuple->HLT_trigPt[i_tr] > 120 && ntuple->HLT_trigPt[i_tr] <= 175)
-                    {
-                        i_120 = i_tr;
-                        matched120 = 1;
-                    }
-                    if (ntuple->HLT_trigName->at(i_tr) == "HLT_Photon175_v*" && ntuple->HLT_trigPt[i_tr] > 175)
-                    {
-                        i_175 = i_tr;
-                        matched175 = 1;
-                    }
-
-                }
-                Int_t prescale_alt = 1;
-                if (matched22+matched30+matched36+matched50+matched75+matched90+matched120+matched175 != 1) continue;
-                if (matched22 == 1)
-                {
-                    prescale_alt = pp.hltPrescale("HLT_Photon22_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG18", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_22], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_22]);
-                }
-                else if (matched30 == 1)
-                {
-                    prescale_alt = pp.hltPrescale("HLT_Photon30_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG26", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_30], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_30]);
-                }
-                else if (matched36 == 1)
-                {
-                    prescale_alt = pp.hltPrescale("HLT_Photon36_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG26", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_36], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_36]);
-                }
-                else if (matched50 == 1)
-                {
-                    prescale_alt = 0;
-                    if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon50_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
-                    if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon50_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_50], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_50]);
-                }
-                else if (matched75 == 1)
-                {
-                    prescale_alt = 0;
-                    if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon75_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
-                    if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon75_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_75], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_75]);
-                }
-                else if (matched90 == 1)
-                {
-                    prescale_alt = 0;
-                    if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon90_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
-                    if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon90_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_90], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_90]);
-                }
-                else if (matched120 == 1)
-                {
-                    prescale_alt = 0;
-                    if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon120_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
-                    if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
-                        prescale_alt = pp.hltPrescale("HLT_Photon120_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_120], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_120]);
-                }
-                else if (matched175 == 1)
-                {
-                     prescale_alt = 0;
-                     if (pp.l1Prescale("L1_SingleEG30", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, lntuple->umiBlock) * pp.l1Prescale("L1_SingleEG30", ntuple->runNum, ntuple->lumiBlock);
-                     if (pp.l1Prescale("L1_SingleEG32", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG32", ntuple->runNum, ntuple->lumiBlock);
-                     if (pp.l1Prescale("L1_SingleEG34", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG34", ntuple->runNum, ntuple->lumiBlock);
-                     if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
-                     if (pp.l1Prescale("L1_SingleEG38", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG38", ntuple->runNum, ntuple->lumiBlock);
-                     if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
-                         prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);                    h_HLT_pT->Fill(trig_pT->at(i_175), prescale_alt);
-                    h_HLT_pT->Fill(ntuple->HLT_trigPt[i_175], prescale_alt);
-                    h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_175]);
-                }
+            Int_t prescale_alt = 1;
+            if (matched22+matched30+matched36+matched50+matched75+matched90+matched120+matched175 != 1) continue;
+            if (matched22 == 1)
+            {
+                prescale_alt = pp.hltPrescale("HLT_Photon22_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG18", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_22], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_22]);
+            }
+            else if (matched30 == 1)
+            {
+                prescale_alt = pp.hltPrescale("HLT_Photon30_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG26", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_30], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_30]);
+            }
+            else if (matched36 == 1)
+            {
+                prescale_alt = pp.hltPrescale("HLT_Photon36_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG26", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_36], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_36]);
+            }
+            else if (matched50 == 1)
+            {
+                prescale_alt = 0;
+                if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon50_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
+                if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon50_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_50], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_50]);
+            }
+            else if (matched75 == 1)
+            {
+                prescale_alt = 0;
+                if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon75_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
+                if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon75_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_75], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_75]);
+            }
+            else if (matched90 == 1)
+            {
+                prescale_alt = 0;
+                if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon90_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
+                if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon90_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_90], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_90]);
+            }
+            else if (matched120 == 1)
+            {
+                prescale_alt = 0;
+                if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon120_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
+                if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
+                    prescale_alt = pp.hltPrescale("HLT_Photon120_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_120], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_120]);
+            }
+            else if (matched175 == 1)
+            {
+                 prescale_alt = 0;
+                 if (pp.l1Prescale("L1_SingleEG30", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, lntuple->umiBlock) * pp.l1Prescale("L1_SingleEG30", ntuple->runNum, ntuple->lumiBlock);
+                 if (pp.l1Prescale("L1_SingleEG32", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG32", ntuple->runNum, ntuple->lumiBlock);
+                 if (pp.l1Prescale("L1_SingleEG34", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG34", ntuple->runNum, ntuple->lumiBlock);
+                 if (pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG36", ntuple->runNum, ntuple->lumiBlock);
+                 if (pp.l1Prescale("L1_SingleEG38", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG38", ntuple->runNum, ntuple->lumiBlock);
+                 if (pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock) != 0)
+                     prescale_alt = pp.hltPrescale("HLT_Photon175_v", ntuple->runNum, ntuple->lumiBlock) * pp.l1Prescale("L1_SingleEG40", ntuple->runNum, ntuple->lumiBlock);                    h_HLT_pT->Fill(trig_pT->at(i_175), prescale_alt);
+                h_HLT_pT->Fill(ntuple->HLT_trigPt[i_175], prescale_alt);
+                h_HLT_pT_uncorr->Fill(ntuple->HLT_trigPt[i_175]);
+            }
+            else continue;
 
             if (DEBUG == kFALSE) bar.Draw(i);
 
